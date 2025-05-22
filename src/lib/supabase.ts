fd9c4b91f927e96bea356fd70aa8659c6eb26ac3
Andrii Gudz <andrey.gudz@gmail.com>
2025-05-22 13:49:34 +0200

    update supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,11 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import 'react-native-url-polyfill/auto'
 
-// Замените на ваши реальные данные из Supabase
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? ''
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl ?? '', supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
